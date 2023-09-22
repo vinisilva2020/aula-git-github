885a8d2d20b0f7daea33316f00022ee47e9076ed
vinisilva2020 <vinicius.2021318590@aluno.iffar.edu.br>
2023-09-22 07:27:56 -0300

    tabela em php

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php print("ola mundo"); ?>
+
+    <table>
+        <thead>
+            <th>id</th>
+            <th>nome</th>
+            <th>contato</th>
+        </thead>
+        <tr>
+            <tbody>
+                <td>1</td>
+                <td>vinicius</td>
+                <td>123456</td>
+            </tbody>
+        </tr>
+    </table>
 </body>
+
 </html>
